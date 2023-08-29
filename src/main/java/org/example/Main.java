ef80fddf5bac5287da665b484d61da6fecd0e154
Mafizul Islam <samsumon@gmail.com>
2023-08-29 17:40:24 -0400

    another commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,5 +15,7 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+
+        System.out.println("Test text!!");
     }
 }
