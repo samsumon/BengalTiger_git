6a96d78ea64823e9e2e8c9067e4ee7b7b0ae0ba1
Mafizul Islam <samsumon@gmail.com>
2023-08-29 17:53:58 -0400

    updated again

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
             System.out.println("My name is Mafi");
             System.out.println("My age is 34");
+            System.out.println("I am a good boy!");
 
         }
     }
